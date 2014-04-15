0cf9ab35fe2703060b263674ed6a589dfd0e5c5c
Spencer Sullivan <sas@fsullivans.net>
2014-04-15 15:36:11 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+var request = require('request');
+var xmldoc = require('xmldoc');
 var express = require("express"),
 	app = express(),
 	port = process.env.PORT || 3000;
+app.engine('html', require('ejs').__express);
 
 var pub = __dirname + '/public',
 	view = __dirname + '/views';
@@ -18,27 +21,27 @@ app.get("/views", function(req, res) {
  	res.render('index.html');
 });
 
-app.get("/display_paths/:base", function (req, res) {
+app.get("/display_paths.html", function (req, res) {
     var api_key = "24be2c9e251e33f63b367301a1011dfd";
-    var base = req.param("base");
-    var options = {
-        hostname: "http://www.veryrelated.com/related-api-v1.php",
-        path: "key=" + api_key + "&base=" + base
-    };
-
-    var gsaReq = http.get(options, function (response) {
-        var completeResponse = '';
-        response.on('data', function (chunk) {
-            completeResponse += chunk;
-        });
-        response.on('end', function() {
-            res.locals.base = base;
-            res.render('display_paths.html')
-        })
-    }).on('error', function (e) {
-        console.log('problem with request: ' + e.message);
+    var base = req.query.base;
+    var hostname = "http://www.veryrelated.com/related-api-v1.php";
+    var path = hostname + "?api_key=" + api_key + "&base=" + base;
+    request(path, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var xml = new xmldoc.XmlDocument(body);
+            var results = xml.childrenNamed("Result");
+            var first = results[0].childNamed("Text").val;
+            var second = results[1].childNamed("Text").val;
+            var third = results[2].childNamed("Text").val;
+            res.render('display_paths.html', {
+                base: base,
+                first: first,
+                second: second,
+                third: third
+            });
+        }
     });
-
 });
+
 
 app.listen(port);
